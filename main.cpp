e293afeffc151440b4716e1601c6ecdbd85d3e0e
Henrik Lievonen <henrik.lievonen@hotmail.com>
2016-10-27 22:02:10 +0300

    Refactor operands to their own functions

--- main.cpp
+++ main.cpp
@@ -3,26 +3,38 @@
 
 using namespace std;
 
+void do_add() {
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+}
+void do_substract() {
+    int a , b;
+    cin >> a >> b;
+    cout << a-b << endl;
+}
+void do_multiply() {
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
+}
+void do_divide() {
+    int a, b;
+    cin >> a >> b;
+    cout << a/b << endl;
+}
+
 int main() {
     string t;
     cin >> t;
 
     if (t == "+" || "add") {
-        // Compute the sum of given numbers
-        int a, b;
-        cin >> a >> b;
-        cout << a+b << endl;
+        do_add();
     } else if (t == "-" || t == "substract") {
-        int a , b;
-        cin >> a >> b;
-        cout << a-b << endl;
+        do_substract();
     } else if (t == "*" || t == "multiply") {
-        int a, b;
-        cin >> a >> b;
-        cout << a*b << endl;
+        do_multiply();
     } else if (t == "/" || t == "divide") {
-        int a, b;
-        cin >> a >> b;
-        cout << a/b << endl;
+        do_divide();
     }
 }
