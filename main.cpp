da4bab570d373717459c2a03c64ea74bbb41b59d
Henrik Lievonen <henrik.lievonen@hotmail.com>
2016-10-27 21:55:29 +0300

    Add support for multiplication

--- main.cpp
+++ main.cpp
@@ -16,5 +16,9 @@ int main() {
         int a , b;
         cin >> a >> b;
         cout << a-b << endl;
+    } else if (t == '*') {
+        int a, b;
+        cin >> a >> b;
+        cout << a*b << endl;
     }
 }
